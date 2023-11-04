3e32cf59c8b38678c0feb273ae7a5ffd387dd14c
bttlfwtr <JeffTan.b@gmail.com>
2023-11-04 03:30:01 -0700

    Redefined Account.hpp

--- include/Account.hpp
+++ include/Account.hpp
@@ -3,28 +3,38 @@
 
 #include <string>
 
-struct Account
+class Account
 {
+public:
    Account(const std::string& u, const std::string& p)
       : username(u), password(p) { }
+   std::string GetUsername() const { return username; }
+   std::string GetPassword() const { return password; }
+
+protected:
    std::string username;
    std::string password;
 };
 
-struct MasterCredentials : Account
+class MasterCredentials : public Account
 {
+public:
    MasterCredentials(const std::string& user, const std::string& pass)
       : Account(user, pass) { }
+
+private:
    std::string hashedUserName;
    std::string hashedPassWord;
    std::string userDefinedKey;
-
 };
 
-struct VaultItem : Account
+class VaultItem : public Account
 {
+public:
    VaultItem(const std::string& user, const std::string& pass, const std::string& d, const std::string& desc, const std::string& t)
       : Account(user, pass), domain(d), description(desc), tag(t) { }
+
+private:
    std::string domain;
    std::string description;
    std::string tag;
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,13 @@
-
+#include <iostream>
+#include "../include/Account.hpp"
 
 int main(int argc, char* argv[])
 {
+   MasterCredentials loggedUser("initialUser", "initialPass");
+   std::cout << loggedUser.GetUsername() << " " << loggedUser.GetPassword() << "\n";
+
+
+
 
 
    return 0;
