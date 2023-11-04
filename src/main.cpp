fba7837ad64f7b6a00cd57a42a5ca121fb73cf5d
bttlfwtr <JeffTan.b@gmail.com>
2023-11-04 03:18:18 -0700

    Setup .gitignore

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,7 @@
-#include <iostream>
-#include "../include/Account.hpp"
+
 
 int main(int argc, char* argv[])
 {
-   MasterCredentials loggedUser("initialUser", "initialPass");
-   std::cout << loggedUser.username << " " << loggedUser.password << "\n";
-
-
-
 
 
    return 0;
