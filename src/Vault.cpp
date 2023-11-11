677cd2cc983b8b9d8a2ec8f4472c5da45fc691f1
zimmshane <80292594+zimmshane@users.noreply.github.com>
2023-11-11 12:46:40 -0800

    create vault.cpp

---
+++ src/Vault.cpp
@@ -0,0 +1,16 @@
+#include "../include/vault.hpp"
+
+bool Vault::addAccount(const VaultItem& vlt){
+
+}
+void Vault::deleteAccount(const std::string& acct){
+//VaultItem vlt = SearchHandler::ReturnReference(acct);
+//PrintHandler::PrintAccount(&vlt)
+//Are you sure you want to delete?
+//delete vlt
+}
+void Vault::modifyAccount(const std::string& acct){
+//VaultItem vlt = SearchHandler::ReturnReference(acct);
+//PrintHandler::PrintAccount(&vlt)
+
+}
