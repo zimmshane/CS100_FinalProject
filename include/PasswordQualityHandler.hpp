ef2feeece90d9cb1ee0360b2929aba380d7e1338
1aaronw <awang236@ucr.edu>
2023-11-12 16:41:52 -0800

    Added PasswordQualityHandler base stubs in src and tests

---
+++ include/PasswordQualityHandler.hpp
@@ -0,0 +1,10 @@
+#ifndef PASSWORDQUALITYHANDLER_HPP
+#define PASSWORDQUALITYHANDLER_HPP
+
+class PasswordQualityHandler {
+    public:
+        bool StrengthChecker(const std::string&);
+        bool RepeatChecker(const std::string&);
+}
+
+#endif // !PASSWORDQUALITYHANDLER_HPP
---
+++ src/PasswordQualityHandler.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "../include/PasswordQualityHandler.hpp"
+
+
+bool PasswordQualityHandler::StrengthChecker(const std::string&) {
+
+}
+bool PasswordQualityHandler::RepeatChecker(const std::string&) {
+
+}
