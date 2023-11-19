4585bbf4d45b6f1823d708e589f9e3f592729cab
zimmshane <zimmshane@gmail.com>
2023-11-18 17:43:24 -0800

    add forward decl, add returnall function

--- include/SearchHandler.hpp
+++ include/SearchHandler.hpp
@@ -4,15 +4,15 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include "Vault.hpp"
-#include "Account.hpp"
 
+class Vault;
+class Account;
+class PasswordManager;
+class VaultItem;
 
 class SearchHandler{
-
 public:
-static std::vector<VaultItem> returnAll(const std::string &search);
-
+static std::vector<VaultItem> returnAll(const std::string &search, const Vault& vlt);
 
 };
 
