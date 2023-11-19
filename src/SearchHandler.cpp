4c752d8fb3f559a53fec7eb602101cf1d711d52d
zimmshane <zimmshane@gmail.com>
2023-11-18 17:44:42 -0800

    implement returnall

--- src/SearchHandler.cpp
+++ src/SearchHandler.cpp
@@ -1,5 +1,22 @@
 #include "../include/SearchHandler.hpp"
+#include "../include/Vault.hpp"
+#include "../include/Account.hpp"
  
-std::vector<VaultItem> SearchHandler::returnAll(const std::string &search){
-    
+std::vector<VaultItem> SearchHandler::returnAll(const std::string &search, const Vault &vlt){
+    std::vector<VaultItem> results;
+    for (auto& entry : vlt.vault){
+        if (entry.first == search){ // if domain matches search, add all accounts to result
+            for (auto it = entry.second.begin(); it != entry.second.end(); ++it){
+                results.push_back((*it));
+            } 
+        } 
+        else{                       //search usernames in domain
+        for (auto it = entry.second.begin(); it != entry.second.end(); ++it){ 
+            if (it->username == search){
+                results.push_back(*it);
+                }
+            }
+        }
+    }
+    return results;
 }
