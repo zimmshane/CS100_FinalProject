6cdceb67fb1cfd4ae230c856d623b734d2bc5a0e
zimmshane <zimmshane@gmail.com>
2023-11-18 17:43:58 -0800

    add forward decl. add 2 new func!

--- include/Vault.hpp
+++ include/Vault.hpp
@@ -1,29 +1,36 @@
 #ifndef VAULT_HPP
 #define VAULT_HPP
 
-#include "Account.hpp"
-// #include <map>
+
 #include <unordered_map>
 #include <vector>
 #include <iostream>
 #include <string>
-#include "Account.hpp"
-#include "PrintHandler.hpp"
-#include "SearchHandler.hpp"
+
+class Account;
+class SearchHandler;
+class VaultItem;
+class PasswordManager;
+class MasterCredential;
+
 
 class Vault
 {
 public:
+  std::unordered_map<std::string, std::vector<VaultItem>> vault;
+
    Vault() { }
    ~Vault() { }
    bool addAccount(const VaultItem&);
    void deleteAccount(const std::string&);
    void modifyAccount(const std::string&);
+   VaultItem findFromSearch(const std::string&);
+   std::vector<VaultItem>::iterator getPositionedItr(const VaultItem& target); //returns a vector iterator at the location of a specific vault item
    
    // put this inside PasswordManager class later
    // std::unordered_map<std::string, std::vector<VaultItem>> GetVault() { return vault; }
 
-   std::unordered_map<std::string, std::vector<VaultItem>> vault;
+ 
 };
 
 #endif // !VAULT_HPP
