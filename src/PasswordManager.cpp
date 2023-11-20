7e85abfa288e40ec78d35450144914b7bd36eda3
zimmshane <zimmshane@gmail.com>
2023-11-20 11:09:13 -0800

    add modify, add, delete calls to menu

--- src/PasswordManager.cpp
+++ src/PasswordManager.cpp
@@ -27,23 +27,18 @@ void PasswordManager::Run_CLI_lock(int argc , char* argv[])
          switch (mainMenuInput)
          {
          case 'A':
-            //TODO: addAccount takes a VaultItem input, how do we we want to get this info?
-            //userVault->addAccount(VAULTITEM)
+            userVault->addAccount(UserInputHandler::GetItemInput()); //thanks jeff!
             std::cout << "ADDED ITEM\n\n" << std::flush;
             break;
          case 'S':
             std::cout << "SEARCHED ITEM\n\n";
             break;
          case 'M':
-            std::string input;
-            UserInputHandler::getStringInput("Enter an domain or account: ",input);
-            userVault->modifyAccount(input);
+            userVault->modifyAccount(UserInputHandler::getStringInput("Search for an account: "));
             std::cout << "MODIFIED ITEM\n\n";
             break;
          case 'D':
-            std::string input;
-            UserInputHandler::getStringInput("Enter an domain or account: ",input);
-            userVault->deleteAccount(input);
+            userVault->deleteAccount(UserInputHandler::getStringInput("Search for an account: "));
             std::cout << "DELETED ITEM\n\n";
             break;
          case 'E':
