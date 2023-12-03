bde5c4c0047c1baa2336b3a8f50efeca5c558a68
zimmshane <zimmshane@gmail.com>
2023-12-02 16:45:40 -0800

    added config file tests

--- tests/Some_Test_Suite.cpp
+++ tests/Some_Test_Suite.cpp
@@ -6,6 +6,8 @@
 #include "../include/PasswordManager.hpp"
 #include "../include/UserInputHandler.hpp"
 #include "../include/Vault.hpp"
+#include <iostream>
+
 
 TEST(ItemContainerTests, VerifyValues)
 {
@@ -74,4 +76,53 @@ TEST(PasswordQualityTests, verifyValidPasswords)
 {
    EXPECT_TRUE(PasswordQualityHandler::StrengthChecker("H3LLoT4ere!"));
    EXPECT_TRUE(PasswordQualityHandler::StrengthChecker("#Th1sworks"));
+}
+
+TEST(ImportConfigTests, canImportConfig){
+   std::ofstream genconfig("TESTconfig.txt");
+   genconfig<<"[GENERATE]\nlength=16\nalpha=9\nnumeric=3\nsymbol=4\n[CURRENT]";
+   Config testConfig;
+   FileHandler::ParseConfig(testConfig,"TESTconfig.txt");
+   EXPECT_TRUE(testConfig.alphaCount==9);
+   EXPECT_TRUE(testConfig.passwordLength==16);
+   EXPECT_TRUE(testConfig.numberCount==3);
+   EXPECT_TRUE(testConfig.symbolCount==4);
+}
+TEST(ImportConfigTests, canHandleMissingValues){
+   std::ofstream genconfig("TESTconfig.txt");
+   genconfig<<"[GENERATE]\nlength=16\nsymbol=4\n[CURRENT]";
+   Config testConfig;
+   EXPECT_ANY_THROW(FileHandler::ParseConfig(testConfig,"TESTconfig.txt")) //check default settings
+   EXPECT_TRUE(testConfig.alphaCount==8);
+   EXPECT_TRUE(testConfig.passwordLength==12);
+   EXPECT_TRUE(testConfig.numberCount==2);
+   EXPECT_TRUE(testConfig.symbolCount==2);
+}
+TEST(ImportConfigTests, canHandleNonNumber){
+   std::ofstream genconfig("TESTconfig.txt");
+   genconfig<<"[GENERATE]\nlength=16\nalpha=NOTANUMBER\nnumeric=3\nsymbol=4\n[CURRENT]";
+   Config testConfig;
+   EXPECT_ANY_THROW(FileHandler::ParseConfig(testConfig,"TESTconfig.txt")) //check default settings
+   EXPECT_TRUE(testConfig.alphaCount==8);
+   EXPECT_TRUE(testConfig.passwordLength==12);
+   EXPECT_TRUE(testConfig.numberCount==2);
+   EXPECT_TRUE(testConfig.symbolCount==2);
+}
+TEST(ImportConfigTests, canHandleBadMath){
+   std::ofstream genconfig("TESTconfig.txt");
+   genconfig<<"[GENERATE]\nlength=16\nalpha=24\nnumeric=45\nsymbol=4\n[CURRENT]";
+   Config testConfig;
+   EXPECT_ANY_THROW(FileHandler::ParseConfig(testConfig,"TESTconfig.txt")) //check default settings
+   EXPECT_TRUE(testConfig.alphaCount==8);
+   EXPECT_TRUE(testConfig.passwordLength==12);
+   EXPECT_TRUE(testConfig.numberCount==2);
+   EXPECT_TRUE(testConfig.symbolCount==2);
+}
+TEST(ImportConfigTests, canHandleNoFile){
+   Config testConfig;
+   EXPECT_ANY_THROW(FileHandler::ParseConfig(testConfig,"TEST2config.txt")) //check default settings
+   EXPECT_TRUE(testConfig.alphaCount==8);
+   EXPECT_TRUE(testConfig.passwordLength==12);
+   EXPECT_TRUE(testConfig.numberCount==2);
+   EXPECT_TRUE(testConfig.symbolCount==2);
 }
