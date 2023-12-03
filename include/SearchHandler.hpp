d6282ac8023440737eaa72c880f0390f1a98eacc
Bryce7832 <bryce.hojo@gmail.com>
2023-12-02 18:37:48 -0800

    Created search function to go through map given the domain

--- include/SearchHandler.hpp
+++ include/SearchHandler.hpp
@@ -1,15 +1,13 @@
 #ifndef SEARCHHANDLER_HPP
 #define SEARCHHANDLER_HPP
 
-#include "Account.hpp"
 #include "Vault.hpp"
 // #include <algorithm>
 
 class SearchHandler
 {
 public:
-    // static void Search(const std::string& domain, const std::string& username); // domain specific username search
-    // static void Search(const std::string& username); // wild card thorough vault search
+    static void Search(const std::string& domainForSearch, const std::string& username, const Vault& vault ); // domain specific username search
     static bool IsUsernameExistInDomainVector(const std::string username, const std::vector<VaultItem>& itemVector); // does username exist in domain key's vector?
     static bool SearchDuplicate(const std::string domain, const Vault& vault); // run through every key domain for duplicate users
     static VaultItem& ReturnReference(const std::string& username); // returns item reference for modification
--- src/SearchHandler.cpp
+++ src/SearchHandler.cpp
@@ -1,13 +1,23 @@
 #include "../include/SearchHandler.hpp"
-#include"../include/Account.hpp"
-// void SearchHandler::Search(const std::string& username)
-// {
 
-//     return;
-// }
+void SearchHandler::Search(const std::string& domainForSearch, const std::string& username, const Vault& vault){
+   std::vector<VaultItem> itemsAtDomain = vault.vaultContainer.at(domainForSearch);
+   for (const auto& iter : itemsAtDomain)
+   {
+      if (iter.username == username)
+      {
+         std::cout << "domain: " << (iter).property.domain << "\n"
+            << "username: " << (iter).username << "\n"
+            << "password: " << (iter).password << "\n"
+            << "description: " << (iter).property.description << "\n"
+            << "tag: " << (iter).property.tag << "\n";
+         return;
+      }
 
-// avoid this by making vault values a map as well for keys of username to avoid all collisions               vvv username string
-// linear search bad, replace data structure later for std::unordered_map<std::string, std::unordered_map<std::string, VaultItem>>
+   }
+   std::cout<<"No account with specified domain\n";
+}
+
 bool SearchHandler::IsUsernameExistInDomainVector(const std::string username, const std::vector<VaultItem>& itemVector)
 {
    for (const auto& iter : itemVector)
