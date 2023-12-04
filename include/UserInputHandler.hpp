899c14d0f15111769183933e47a578d252be79c1
bttlfwtr <JeffTan.b@gmail.com>
2023-12-04 00:33:44 -0800

    remove empty functions

--- include/UserInputHandler.hpp
+++ include/UserInputHandler.hpp
@@ -15,11 +15,7 @@ public:
 
    // prevent leading or trailing for all
    static VaultItem GetItemInput();
-   static void GetItemDomain(std::string& itemDomain);
-   static void GetItemUsername(std::string& itemUsername); // validate username down to a string without space and maximum length of 64
    static void GetItemPassword(std::string& itemPassword); // validate password down to 32
-   static void GetItemDescription(std::string& itemDescription); // no other constraints
-   static void GetItemTag(std::string& itemTag); // no between spaces
    static void GetGenericInput(const std::string& msg, std::string& field);
 };
 
