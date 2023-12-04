0d68a03c8357419279e445787679575ab5cc5ed9
bttlfwtr <JeffTan.b@gmail.com>
2023-12-03 22:52:14 -0800

    setup hash function using Scrypt KDF

--- tests/cipher_testing.cpp
+++ tests/cipher_testing.cpp
@@ -8,11 +8,11 @@ std::string StringToSHA256(const std::string& input)
    std::string defaultSalt {"NaCl OMEGALUL"};
 
    // std::string --> bytes of data/hex for hashing
-   const CryptoPP::byte* pass = reinterpret_cast<const CryptoPP::byte*>(input.data());
-   const CryptoPP::byte* saltBytes = reinterpret_cast<const CryptoPP::byte*>(defaultSalt.data());
+   const CryptoPP::byte* pass {reinterpret_cast<const CryptoPP::byte*>(input.data())};
+   const CryptoPP::byte* saltBytes {reinterpret_cast<const CryptoPP::byte*>(defaultSalt.data())};
 
    // buffer byte block for output of hash
-   CryptoPP::SecByteBlock derivedKey(64);
+   CryptoPP::SecByteBlock derivedKey(32);
 
    // instantiate object to utilize class functions
    CryptoPP::Scrypt scrypt;
@@ -20,11 +20,13 @@ std::string StringToSHA256(const std::string& input)
    // perform key derivation function
    scrypt.DeriveKey(derivedKey, derivedKey.size(), pass, input.length(), saltBytes, defaultSalt.length(), 1024, 8, 16);
 
-   // Convert the derived key to a hexadecimal string
-   std::string hexDerived;
-   CryptoPP::StringSource(derivedKey, 16, true, new CryptoPP::HexEncoder(new CryptoPP::StringSink(hexDerived)));
+   // assign the derived key to a string as an encoded hex string
+   // if derived key byte size = 64, then derived is 128 hex chars
+   // per byte == hex pair
+   std::string derivedKeyHexValue;
+   CryptoPP::StringSource(derivedKey, derivedKey.size(), true, new CryptoPP::HexEncoder(new CryptoPP::StringSink(derivedKeyHexValue)));
 
-   return hexDerived;
+   return derivedKeyHexValue;
 }
 
 #include <iostream>
